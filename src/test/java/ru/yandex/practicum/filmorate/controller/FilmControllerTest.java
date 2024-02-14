9d0ee2fd6a825ad72ed0edffe34a0031792f12df
Indira <indira-zakirova@mail.ru>
2024-02-14 20:51:48 +0300

    feat: 1 ревью

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FilmControllerTest {
     FilmController filmController = new FilmController();
+
     @Test
     void shouldAddFilm() {
         Film film1 = new Film();
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -16,17 +16,17 @@ public class UserControllerTest {
         User user = new User();
         user.setEmail("friend.ru");
         user.setLogin("friend");
-        user.setBirthday(LocalDate.of(1996, 8,19));
+        user.setBirthday(LocalDate.of(1996, 8, 19));
 
         User user1 = new User();
         user1.setEmail("test@yandex.ru");
         user1.setLogin("test");
-        user1.setBirthday(LocalDate.of(2027, 8,19));
+        user1.setBirthday(LocalDate.of(2027, 8, 19));
 
         User user2 = new User();
         user2.setEmail("user@yandex.ru");
         user2.setLogin("best user");
-        user2.setBirthday(LocalDate.of(1996, 7,16));
+        user2.setBirthday(LocalDate.of(1996, 7, 16));
 
 
         assertThrows(ValidationException.class, () -> userController.addUser(user));
