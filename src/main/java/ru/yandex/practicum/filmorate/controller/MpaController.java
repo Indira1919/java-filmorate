b9c02fe58eca69925a1dc25f82aede02eb15d911
Indira <indira-zakirova@mail.ru>
2024-03-29 17:52:34 +0300

    feat: работа с БД

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.MpaService;
 
 import java.util.Collection;
-import java.util.List;
 
 @RestController
 @Slf4j
